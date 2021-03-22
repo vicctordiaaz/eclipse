d12905e34634b3b879c1776b7ba60337b1a5074c
Victor <victordiaznombela@gmail.com>
2021-03-22 11:06:14 +0100

    Ejercicio 8.2

---
+++ Proyecto2PHP/ecuacion-1-grado.php
@@ -0,0 +1,16 @@
+<?php 
+$a=$_POST['var1'];
+$b=$_POST['var2'];
+
+if ($a == 0) {
+    echo "Esa ecuación no tiene solución real.";
+} else {
+    echo "x = ", (-$b / $a);
+}
+?>
+
+<form action="ecuacion-1-grado.php" method="POST">
+	Numero1: <input type="text" name="var1" size="10" value="var1"/>
+	Numero2: <input type="text" name="var2" size="10" value="var2"/>
+	<input type="submit" value="Solucion" name="subSolucion"/>
+</form>
