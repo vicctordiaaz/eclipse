8d86979a1f358cd2ec3a626ec56987b66db9fc8f
Victor <victordiaznombela@gmail.com>
2021-03-22 11:10:20 +0100

    Ejercicio 8.1

--- Proyecto2PHP/ejercicio7.php
+++ Proyecto2PHP/ejercicio7.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Ejercicio 8.1</title>
+	<title>Ejercicio1</title>
 	<style type="text/css">
 		input [type=text], input[type=submit], select{
 			text-align:center;
@@ -12,32 +12,55 @@
 </head>
 <body>
 <?php
-	$numero1="";
-	$numero2="";
-	$suma="";
-	$mensaje="";
+
+	$numero1 = "";
+	$numero2 = "";
+	$suma = "";
+	$mensaje = "";
+	$factorial = "";
+
+	$numeroF = "";
 
 	if (isset($_POST['subSumar'])) {
-		$numero1=$_POST['txtN1'];
-		$numero2=$_POST['txtN2'];
-		
-		if(is_numerio($numero1) && is_numerio($numero2)){
+		$numero1 = $_POST['txtN1'];
+		$numero2 = $_POST['txtN2'];
+		if (is_numeric($numero1) && is_numeric($numero2)){
 			$suma = $numero1 + $numero2;
-			
 		} else {
-		$numero1="";
-		$numero2="";
-		$suma="";
-		$mensaje="Datos incorrectos";
+		$numero1 = "";
+		$numero2 = "";
+		$suma = "";
+		$mensaje = "Datos incorrectos";
+		}
+	}
+
+	if (isset($_POST['subFactorial'])) {
+		$numeroF = $_POST['txtN1factorial'];
+		$factorial = 1;
+		if (is_numeric($numeroF)){
+			for ($i = 1; $i <= $numeroF; $i++){
+				$factorial = $factorial * $i;
+			}
+		} else {
+		$numeroF = "";
+		$fact = "";
+		$mensaje = "Datos incorrectos";
 		}
 	}
 ?>
-
-<form method="POST">
+<form action="ejercicio.php" method="POST">
 	Numero1: <input type="text" name="txtN1" size="10" value="<?php echo $numero1; ?>"/>
 	Numero2: <input type="text" name="txtN2" size="10" value="<?php echo $numero2; ?>"/>
 	Resultado: <input type="text" name="txtR" size="10" value="<?php echo $suma; ?>" disabled/>
 	Mensaje: <input type="text" name="txtR" size="15" value="<?php echo $mensaje; ?>" disabled/>
 	<input type="submit" value="Sumar" name="subSumar"/>
 </form>
+<br>
+<form action="ejercicio.php" method="POST">
+	Numero1: <input type="text" name="txtN1factorial" size="10" value="<?php echo $numeroF; ?>"/>
+	Resultado: <input type="text" name="txtR" size="10" value="<?php echo $fact; ?>" disabled/>
+	Mensaje: <input type="text" name="txtR" size="15" value="<?php echo $mensaje; ?>" disabled/>
+	<input type="submit" value="Fact" name="subFactorial"/>
+</form>
+</body>
 </html>
